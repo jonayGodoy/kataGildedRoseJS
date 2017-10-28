61f19fd2564b99decf2d82d5c1569874613a4aa9
jonay <casa@gmail.com>
2017-10-28 04:55:27 +0100

    refactor remove new GildedRose

--- solution/spec/GildedRose.spec.js
+++ solution/spec/GildedRose.spec.js
@@ -20,7 +20,7 @@ describe("Gilded Rose",function(){
     it('item decrement sellIn', function () {
         let item = Item("anyItem",2,20);
 
-        new GildedRose([item]).updateQuality();
+        GildedRose([item]).updateQuality();
 
         item.sellIn.should.equal(1);
     });
@@ -28,7 +28,7 @@ describe("Gilded Rose",function(){
     it('quality item never is negative', function () {
         let item = Item("anyItem",0,0);
 
-        new GildedRose([item]).updateQuality();
+        GildedRose([item]).updateQuality();
 
         item.quality.should.equal(0);
     });
@@ -36,7 +36,7 @@ describe("Gilded Rose",function(){
     it('once sell date has passes quality degrade twice fast', function () {
         let item = Item("anyItem",0,4);
 
-        new GildedRose([item]).updateQuality();
+        GildedRose([item]).updateQuality();
 
         item.quality.should.equal(2);
     });
@@ -44,7 +44,7 @@ describe("Gilded Rose",function(){
     it('sulfuras never decrement quality', function () {
         let item = Item("Sulfuras, Hand of Ragnaros",20,80);
 
-        new GildedRose([item]).updateQuality();
+        GildedRose([item]).updateQuality();
 
         item.quality.should.equal(80);
     });
@@ -52,7 +52,7 @@ describe("Gilded Rose",function(){
     it('backstage passes quality increase by 2 there are 10 days or less', function () {
         let item = Item("Backstage passes to a TAFKAL80ETC concert",10,4);
 
-        new GildedRose([item]).updateQuality();
+        GildedRose([item]).updateQuality();
 
         item.quality.should.equal(6);
     });
@@ -60,7 +60,7 @@ describe("Gilded Rose",function(){
     it('backstage passes quality increase by 3 there are 5 days or less', function () {
         let item = Item("Backstage passes to a TAFKAL80ETC concert",5,4);
 
-        new GildedRose([item]).updateQuality();
+        GildedRose([item]).updateQuality();
 
         item.quality.should.equal(7);
     });
@@ -68,7 +68,7 @@ describe("Gilded Rose",function(){
     it('backstage passes quality 0 when sellIn 0', function () {
         let item = Item("Backstage passes to a TAFKAL80ETC concert",0,50);
 
-        new GildedRose([item]).updateQuality();
+        GildedRose([item]).updateQuality();
 
         item.quality.should.equal(0);
     });
