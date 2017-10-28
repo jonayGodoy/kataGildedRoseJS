bccb5968f2e568484b4c226d29dfbe3bc5a41a54
jonay <casa@gmail.com>
2017-10-28 04:49:21 +0100

    red: sulfuras never decrement quality

--- issue/src/Item.js
+++ issue/src/Item.js
@@ -1,6 +1,13 @@
-function Item() {
-
-}
+function Item(name ,sellIn , quality) {
+    return {
+        "name": name,
+        "sellIn": sellIn,
+        "quality": quality,
+        "print": function print() {
+            return name + ", " + sellIn + ", " + quality;
+        }
+    }
+};
 
 
 module.exports = Item;
--- solution/spec/GildedRose.spec.js
+++ solution/spec/GildedRose.spec.js
@@ -12,84 +12,64 @@ describe("Gilded Rose",function(){
 
     it('item decrement quality', function () {
         let item = new Item("anyItem",20,2);
+        new GildedRose([item]).updateQuality();
 
-        let items = {item};
-        new GildedRose(items).updateQuality();
-
-        item.getQuality().should.equal(1);
+        item.quality.should.equal(1);
     });
 
     it('item decrement sellIn', function () {
         let item = new Item("anyItem",2,20);
 
-        let items = {item};
-        new GildedRose(items).updateQuality();
+        new GildedRose([item]).updateQuality();
 
-        item.getSellIn().should.equal(1);
+        item.sellIn.should.equal(1);
     });
 
     it('quality item never is negative', function () {
         let item = new Item("anyItem",0,0);
 
-        let items = {item};
-        new GildedRose(items).updateQuality();
+        new GildedRose([item]).updateQuality();
 
-        item.getQuality().should.equal(0);
+        item.quality.should.equal(0);
     });
 
     it('once sell date has passes quality degrade twice fast', function () {
         let item = new Item("anyItem",0,4);
 
-        let items = {item};
-        new GildedRose(items).updateQuality();
+        new GildedRose([item]).updateQuality();
 
-        item.getQuality().should.equal(2);
+        item.quality.should.equal(2);
     });
 
     it('sulfuras never decrement quality', function () {
-        let item = new Sulfuras("Sulfuras, Hand of Ragnaros",20);
+        let item = new Item("Sulfuras, Hand of Ragnaros",20);
 
+        new GildedRose([item]).updateQuality();
 
-        let items = {item};
-        new GildedRose(items).updateQuality();
-
-        item.getQuality().should.equal(80);
+        item.quality.should.equal(80);
     });
 
     it('backstage passes quality increase by 2 there are 10 days or less', function () {
-        let item = new Backstage("Backstage passes to a TAFKAL80ETC concert",10,4);
+        let item = new Item("Backstage passes to a TAFKAL80ETC concert",10,4);
 
-        let items = {item};
-        new GildedRose(items).updateQuality();
+        new GildedRose([item]).updateQuality();
 
-        item.getQuality().should.equal(6);
+        item.quality.should.equal(6);
     });
 
     it('backstage passes quality increase by 3 there are 5 days or less', function () {
-        let item = new Backstage("Backstage passes to a TAFKAL80ETC concert",5,4);
+        let item = new Item("Backstage passes to a TAFKAL80ETC concert",5,4);
 
-        let items = {item};
-        new GildedRose(items).updateQuality();
+        new GildedRose([item]).updateQuality();
 
-        item.getQuality().should.equal(7);
+        item.quality.should.equal(7);
     });
 
     it('backstage passes quality 0 when sellIn 0', function () {
-        let item = new Backstage("Backstage passes to a TAFKAL80ETC concert",0,50);
+        let item = new Item("Backstage passes to a TAFKAL80ETC concert",0,50);
 
-        let items = {item};
-        new GildedRose(items).updateQuality();
+        new GildedRose([item]).updateQuality();
 
-        item.getQuality().should.equal(0);
-    });
-
-
-    it('conjured updateQuality decrease quality by 2', function () {
-        let item = new Conjured("Conjured",10,50);
-
-        let items = {item};
-        new GildedRose(items).updateQuality();
-
-        item.getQuality().should.equal(48);
+        item.quality.should.equal(0);
     });
 });
