d54cd4659c3f798f11dc4c66750be858aba1b09b
jonay <casa@gmail.com>
2017-10-28 04:53:15 +0100

    refactor remove new Item

--- solution/spec/GildedRose.spec.js
+++ solution/spec/GildedRose.spec.js
@@ -11,14 +11,14 @@ describe("Gilded Rose",function(){
     });
 
     it('item decrement quality', function () {
-        let item = new Item("anyItem",20,2);
+        let item = Item("anyItem",20,2);
         new GildedRose([item]).updateQuality();
 
         item.quality.should.equal(1);
     });
 
     it('item decrement sellIn', function () {
-        let item = new Item("anyItem",2,20);
+        let item = Item("anyItem",2,20);
 
         new GildedRose([item]).updateQuality();
 
@@ -26,7 +26,7 @@ describe("Gilded Rose",function(){
     });
 
     it('quality item never is negative', function () {
-        let item = new Item("anyItem",0,0);
+        let item = Item("anyItem",0,0);
 
         new GildedRose([item]).updateQuality();
 
@@ -34,7 +34,7 @@ describe("Gilded Rose",function(){
     });
 
     it('once sell date has passes quality degrade twice fast', function () {
-        let item = new Item("anyItem",0,4);
+        let item = Item("anyItem",0,4);
 
         new GildedRose([item]).updateQuality();
 
@@ -42,7 +42,7 @@ describe("Gilded Rose",function(){
     });
 
     it('sulfuras never decrement quality', function () {
-        let item = new Item("Sulfuras, Hand of Ragnaros",20,80);
+        let item = Item("Sulfuras, Hand of Ragnaros",20,80);
 
         new GildedRose([item]).updateQuality();
 
@@ -50,7 +50,7 @@ describe("Gilded Rose",function(){
     });
 
     it('backstage passes quality increase by 2 there are 10 days or less', function () {
-        let item = new Item("Backstage passes to a TAFKAL80ETC concert",10,4);
+        let item = Item("Backstage passes to a TAFKAL80ETC concert",10,4);
 
         new GildedRose([item]).updateQuality();
 
@@ -58,7 +58,7 @@ describe("Gilded Rose",function(){
     });
 
     it('backstage passes quality increase by 3 there are 5 days or less', function () {
-        let item = new Item("Backstage passes to a TAFKAL80ETC concert",5,4);
+        let item = Item("Backstage passes to a TAFKAL80ETC concert",5,4);
 
         new GildedRose([item]).updateQuality();
 
@@ -66,7 +66,7 @@ describe("Gilded Rose",function(){
     });
 
     it('backstage passes quality 0 when sellIn 0', function () {
-        let item = new Item("Backstage passes to a TAFKAL80ETC concert",0,50);
+        let item = Item("Backstage passes to a TAFKAL80ETC concert",0,50);
 
         new GildedRose([item]).updateQuality();
 
